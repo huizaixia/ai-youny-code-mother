234ad4ad5ce6589fec6ab7844d8db7cd42f03078
huizaixia <1053978523@qq.com>
2026-08-19 20:45:32 +0800

    项目初始化

---
+++ src/main/java/com/study/youny/YounyCodeMotherApplication.java
@@ -0,0 +1,11 @@
+package com.study.youny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YounyCodeMotherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YounyCodeMotherApplication.class, args);
+    }
+}
---
+++ src/main/java/com/study/youny/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.study.youny.controller;
+
+import com.study.youny.common.BaseResponse;
+import com.study.youny.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
